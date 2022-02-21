5f082c8e6d056c539f7aa0eca77d3db4b55b1cfd
Santiago Faci <santi@codeandcoke.com>
2022-02-21 20:32:51 +0100

    probando

--- src/main/java/Principal.java
+++ src/main/java/Principal.java
@@ -1,5 +1,30 @@
 public class Principal {
     public static void main(String[] args) {
         System.out.println("QUé tal?");
+        System.out.println("Uno");
     }
+
+
+
+    //Rama morada
+    public void login(String usuario, String password){
+
+    }
+    public void signup(String usuario, String password){
+        String consulta = "INSERT (usuarios, password) VALUES("+usuario+password+") FROM Usuarios";
+        //llamar a la bbdd
+    }
+
+
+    //Fin Rama morada
+
+    //Rama verde
+    public void login(String usuario, String password){
+        String consulta = "SELECT * FROM Usuarios WHERE nombre="+usuario+" AND password="+password;
+        //llamar a la bbdd
+    }
+    public void signup(String usuario, String password){
+    }
+    //Fin Rama Verde
 }
+
